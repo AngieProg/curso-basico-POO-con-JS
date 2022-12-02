b064730e10ba6367a7b3a2dbb61a1882d6775f6c
AngieProg <angie_db@hotmail.com>
2022-12-01 18:33:14 -0600

    Prototipos con la sintaxis de clases, constructor como objeto en donde no importa el orden de los parametros, se pueden inicializar o pueden venir vacios sin necesidad de comillas vacias

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Creando un objeto literal
+
 const natalia = {
     name: "Natalia",
     age: 20,
@@ -21,11 +23,11 @@ const natalia = {
 //Hacer que Natalia apruebe otro curso
 // natalia.cursosAprobados.push("Curso de responsive design");
 
+//Creando un prototipo 
 function Student(name, age, cursosAprobados) {
     this.name = name;
     this.age = age;
     this.cursosAprobados = cursosAprobados;
-
     //Forma 1 de guardar un método
     /*this.aprobarCurso = function(nuevoCursito){
         this.cursosAprobados.push(nuevoCursito);
@@ -45,3 +47,47 @@ const juanita = new Student(
         "Curso de creación de personajes",
     ],
 );  
+
+//Prototipos con la sintaxis de clases  
+class Student2 {
+    /*constructor(name, age, cursosAprobados){
+        
+    }*/
+
+    constructor({
+        name,
+        age,
+        cursosAprobados = [],
+        email,
+    }){
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.cursosAprobados = cursosAprobados;
+    }
+
+    aprobarCurso(nuevoCursito){
+        this.cursosAprobados.push(nuevoCursito);
+    }
+}
+
+/*const miguelito = new Student2(
+    "Miguel",
+    28,
+    [
+        "Curso Análisis de Negocios para Ciencias de Datos",
+        "Cursos de Principios de Visualización de Datos para BI",
+    ], 
+);*/
+
+const miguelita = new Student2({
+    email: "miguelita@gmail.com",
+    age: 23,
+    name: "Miguelita",
+});
+
+
+
+
+
+
