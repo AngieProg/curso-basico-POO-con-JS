2ee98073f51c74bdc413000cabd242505d102bf6
AngieProg <angie_db@hotmail.com>
2022-12-01 21:25:09 -0600

    Comparamos objetos literales vs prototipos con sintaxis de clases e instancias de prototipos

--- main.js
+++ main.js
@@ -86,7 +86,7 @@ const miguelita = new Student2({
     name: "Miguelita",
 });
 
-
+// CLASE 7: COMPARANDO OBJETOS LITERALES, PROTOTIPOS, PROTOTIPOS CON SINTAXIS DE CLASES E INSTANCIAS DE PROTOTIPOS
 //Creando objeto literal
 const juanLiteral = {
     name: "JuanDC",
@@ -122,8 +122,123 @@ const juanLiteral = {
             ]   
         },
     ],
-
 }
 
+//Creando objeto literal
+const miguelitoLiteral = {
+    name: "Miguelito",
+    username: "miguelitofeliz",
+    points: 1000,
+    //Podemos guardar objetos dentro de objetos como es el caso de socialMedia
+    socialMedia : {
+        twitter: "miguelitofeliz",
+        instaram: "miguelito_feliz",
+        facebook: undefined,
+    },
+    //Guardamos esta lista en un array
+    approvedCourses: [
+        "Curso DataBusiness",
+        "Curso DataViz",
+    ],
+    //Creamos un array de objetos
+    learningPaths: [
+        {
+            name: "Escuela de desarrollo Web",
+            courses: [
+                "Curso Definitivo de HTML y CSS",
+                "Curso Práctico de HTML y CSS",
+                "Curso de Responsive Design"
+            ]
+        },
+        {
+            name: "Escuela de DataScience",
+            courses: [
+                "Curso DataBusiness",
+                "Curso DataViz",
+                "Curso Tableau",
+            ]   
+        },
+    ],
+}
 
+class StudentClass {
+    constructor({
+    name,
+    email,
+    username,
+    twitter = undefined,
+    instagram = undefined,
+    facebook = undefined,
+    approvedCourses = [],
+    learningPaths = [],
+}){
+    this.name = name;
+    this.email = email;
+    this.username = username;
+    this.socialMedia = {
+        twitter:twitter,
+        //twitter  -> JS nos permite este tipo de sintaxis para abreviar la de arriba
+        instagram,
+        facebook,
+    };
+    this.approvedCourses =approvedCourses;
+    this.learningPaths = learningPaths;
+    }
+}
 
+const JuanProto = new StudentClass({
+    name: "Juan DC",
+    username: "juandc",
+    email: "juanito@gmail.com",
+    twitter: "fjuandc",
+    learningPaths: [
+        escuelaWeb,
+        escuelaVgs,
+    ]
+});
+
+const MiguelitoProto = new StudentClass({
+    name: "Miguelito",
+    username: "miguelito_felix",
+    email: "miguelito@gmail.com",
+    instagram: "miguelitofeliz",
+    learningPaths: [
+        escuelaWeb,
+        escuelaDataScience,
+    ]
+});
+
+class LearningPath {
+    constructor({
+        name,
+        courses = [],
+    }){
+        this.name = name;
+        this.courses = courses;
+        }
+}
+
+const escuelaWeb = new LearningPath({
+    name: "Escuela de Desarrollo Web",
+    courses:[
+        "Curso Definitivo de HTML y CSS",
+        "Curso Práctico de HTML y CSS",
+        "Curso de Responsive Design",
+    ]
+});
+const escuelaDataScience = new LearningPath({
+    name: "Escuela de Data Science",
+    courses:[
+        "Curso DataBusiness",
+        "Curso DataViz",
+        "Curso Tableau",
+    ]
+});
+const escuelaVgs = new LearningPath({
+    name: "Escuela de Videojuegos",
+    courses:[
+        "Curso Introducción a la Producción de Videojuegos",
+        "Curso de Unreal Engine",
+        "Curso de Unioty 3D",
+    ]
+});
